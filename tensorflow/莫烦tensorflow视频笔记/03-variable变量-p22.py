3b3c3249109c3c8da3d89bc3d4dfd8beb65756a5
wangbin122120 <wangbin122120@163.com>
2017-08-15 18:08:39 +0800

    add

--- tensorflow/莫烦tensorflow视频笔记/03-variable变量-p22.py
+++ tensorflow/莫烦tensorflow视频笔记/03-variable变量-p22.py
@@ -10,3 +10,16 @@ update = tf.assign(state, new_value)  # 将new_value当前的状态复制到stat
 # 若定义了Variable,那么必定有一步定义：
 init = tf.global_variables_initializer()
 
+with tf.Session() as sess:
+    sess.run(init)
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state)) #必须要run()之后才会出结果
+
+'''输出结果：
+1
+2
+3
+
+Process finished with exit code 0
+'''
--- tensorflow/莫烦tensorflow视频笔记/练习/01-tensorflow示例-p20.py
+++ tensorflow/莫烦tensorflow视频笔记/练习/01-tensorflow示例-p20.py
@@ -6,8 +6,8 @@ x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data * 0.1 + 0.3
 
 # create model
-Weights = tf.random_uniform([1], -1, 1)
-b = tf.zeros([1])
+Weights = tf.Variable(tf.random_uniform([1], -1, 1))
+b = tf.Variable(tf.zeros([1]))
 y = Weights * x_data + b
 
 loss = tf.reduce_mean(tf.square(y - y_data))
