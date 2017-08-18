835f8808455a9c1cbfeaeac57a3926a446f8d5e5
wangbin122120 <wangbin122120@163.com>
2017-08-18 10:49:44 +0800

    2017-8-17 12:08:45

--- tensorflow/莫烦tensorflow视频笔记/p25_def添加层.py
+++ tensorflow/莫烦tensorflow视频笔记/p25_def添加层.py
@@ -6,18 +6,23 @@ gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
 config.gpu_options.allow_growth = True  # 开始不会给tensorflow全部gpu资源 而是按需增加
 sess = tf.Session(config=config)
 
+
 # 定义一个
-def add_layer(inputs, in_size, out_size, activation_function=None):
+def add_layer(inputs, in_size, out_size, activation_function=None, layer_name='layer'):
     # W和b在初始时为0是很不好的，随机生成。
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]),dtype=tf.float32)
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    # 在p29课中添加name_scope:
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), dtype=tf.float32, name='W')
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        # @注意此句不等于： Wx_plus_b = tf.add(tf.multiply(Weights, inputs), biases)
 
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
-    #@注意此句不等于： Wx_plus_b = tf.add(tf.multiply(Weights, inputs), biases)
-
-    # 如果没有定义激励函数，那么就用初始的线性函数，否则多加一层则增加一个非线性函数
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
-    return outputs
+        # 如果没有定义激励函数，那么就用初始的线性函数，否则多加一层则增加一个非线性函数
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)  # 这里不需要处理名字，因为每个激励函数自身都有
+        return outputs
---
+++ tensorflow/莫烦tensorflow视频笔记/练习/ok/p29_Tensorboard可视化.py
@@ -0,0 +1,65 @@
+import matplotlib.pylab as plt
+
+# 是《p26_建造神经网络》程序的例子
+
+import numpy as np
+import tensorflow as tf
+
+# 限制显卡内存
+config = tf.ConfigProto(allow_soft_placement=True)
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+config.gpu_options.allow_growth = True  # 开始不会给tensorflow全部gpu资源 而是按需增加
+sess = tf.Session(config=config)
+
+from p25_def添加层 import add_layer
+
+# 设定输入数据
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]  # ，在最后一个维度后增加维度。
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+# # 建立典型的三层神经网络:输入层-隐藏层-输出层
+
+# 设定参数，要明白设置placeholder，是为了提升训练的效率,因为不需要用到所有数据
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+
+# @神经网络最关键的两步：
+# 设置隐藏层，输入个数是由xs决定，个数是1，设定给10个神经元，其激励函数用relu
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+# 设置输出层，输入个数是由上一层l1决定，个数是10，输出结果只有一个，输出层不需要设定激励函数
+predition = add_layer(l1, 10, 1, activation_function=None)
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - predition),
+                                    reduction_indices=1))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init = tf.global_variables_initializer()
+
+# 可视化过程：
+# 这段程序最好放在外侧，
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()  # 有了这句话，在画图的过程中不会中断程序，不需要人工干预就自动接着执行。 对应的关闭方法：plt.ioff()
+plt.show()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(1000):
+        sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+        if i % 50 == 0:
+            loss_value = sess.run(loss, feed_dict={xs: x_data, ys: y_data})
+            predition_value=sess.run(predition,feed_dict={xs:x_data})
+
+            # 因为循环过程会不断产生lines,在画新lines时，应该先将旧lines删除，由于第一次删除时没有线条，所以会报错，跳过即可。这样最后程序结束时正好保留最终的预测曲线。
+            try:
+                ax.lines.remove(lines[0])
+                # @不要错写成：fig.remove(lines[0])，这样删不掉的。
+            except Exception:
+                pass
+            plt.xlabel('loss=%.5f' % loss_value)
+            lines = ax.plot(x_data,predition_value,'r-',lw=5) #lw线条宽度。在ax图片上画预测曲线，因为后面要清除，所以需要一个返回值lines。
+            plt.pause(0.1)
+
