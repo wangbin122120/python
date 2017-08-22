ec9603c3e458be8bbe392cfecd519f06d6341f6e
wangbin122120 <wangbin122120@163.com>
2017-08-22 16:50:01 +0800

    2017-08-19 08:52:54

--- tensorflow/莫烦tensorflow视频笔记/练习/p36_saver保存读取（神经网络）.py
+++ tensorflow/莫烦tensorflow视频笔记/练习/p36_saver保存读取（神经网络）.py
@@ -1,10 +1,28 @@
 import tensorflow as tf
+
 # 限制显卡显存
 config = tf.ConfigProto(allow_soft_placement=True)
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
-config.gpu_options.allow_growth = True #开始不会给tensorflow全部gpu资源 而是按需增加
+config.gpu_options.allow_growth = True  # 开始不会给tensorflow全部gpu资源 而是按需增加
 sess = tf.Session(config=config)
+#
+# # 保存数据
+# W = tf.Variable([[1, 2, 3], [2, 3, 4]], dtype=tf.float32)
+# b = tf.Variable([1, 2, 3], dtype=tf.float32)
+#
+# save = tf.train.Saver()
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#     print('path:', save.save(sess, '/tmp/tensorflow/mofan_logs/p36/save2.ckpt'))
 
-#保存数据
-W=tf.Variable([[1,2,3],[2,3,4]],dtype=tf.float32)
-b=tf.Variable([1,2,3],dtype=tf.float32)
+
+# 提取数据
+import numpy as np
+W=tf.Variable(np.arange(6).reshape((2,3)),dtype=tf.float32)
+b=tf.Variable(np.arange(3).reshape((1,3)),dtype=tf.float32)
+
+save=tf.train.Saver()
+with tf.Session() as sess:
+    save.restore(sess,'/tmp/tensorflow/mofan_logs/p36/save2.ckpt')
+    print('已经提取完毕：\nW=\n%s\nb=\n%s' % (sess.run(W), sess.run(b)))
