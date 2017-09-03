86cb5619061e9552fa8d8427b15489fb20233967
wangbin122120 <wangbin122120@163.com>
2017-09-03 14:42:36 +0800

    99

--- m2.py
+++ m2.py
@@ -1,38 +1,42 @@
-import tensorflow as tf
-# NumPy is often used to load, manipulate and preprocess data.
 import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
-# Declare list of features. We only have one numeric feature. There are many
-# other types of columns that are more complicated and useful.
-feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
+tf.logging.set_verbosity(tf.logging.ERROR)
 
-# An estimator is the front end to invoke training (fitting) and evaluation
-# (inference). There are many predefined types like linear regression,
-# linear classification, and many neural network classifiers and regressors.
-# The following code provides an estimator that does linear regression.
-estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+image_size = 28
+labels_size = 10
+hidden_size = 1024
 
-# TensorFlow provides many helper methods to read and set up data sets.
-# Here we use two data sets: one for training and one for evaluation
-# We have to tell the function how many batches
-# of data (num_epochs) we want and how big each batch should be.
-x_train = np.array([1., 2., 3., 4.])
-y_train = np.array([0., -1., -2., -3.])
-x_eval = np.array([2., 5., 8., 1.])
-y_eval = np.array([-1.01, -4.1, -7, 0.])
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
-eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+# Read in the MNIST dataset
+mnist = input_data.read_data_sets("/home/w/tmp/tensorflow/mnist/input_data", one_hot=False)
 
-# We can invoke 1000 training steps by invoking the  method and passing the
-# training data set.
-estimator.train(input_fn=input_fn, steps=1000)
+def input_fn(dataset):
+    features = dataset.images
+    labels = dataset.labels.astype(np.int32)
+    return features, labels
 
-# Here we evaluate how well our model did.
-train_metrics = estimator.evaluate(input_fn=train_input_fn)
-eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
-print("train metrics: %r"% train_metrics)
-print("eval metrics: %r"% eval_metrics)
+# Define the Estimator
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=image_size*image_size)]
+classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                              hidden_units=[hidden_size],
+                                              n_classes=labels_size,
+                                              optimizer=tf.train.AdamOptimizer())
+
+# Fit the model
+features, labels = input_fn(mnist.train)
+classifier.fit(x=features, y=labels, batch_size=100, steps=1000)
+
+# Evaluate the model on the test data
+features, labels = input_fn(mnist.test)
+test_accuracy = classifier.evaluate(x=features, y=labels, steps=1)["accuracy"]
+
+print("\nTest accuracy: %g %%"%(test_accuracy*100))
+
+# Predict the new examples and compare with the onderlying values
+features = mnist.validation.images[:10]
+labels = mnist.validation.labels[:10].astype(np.int32)
+predictions = list(classifier.predict(x=features))
+
+print("\nPredicted labels from validation set: %s"%predictions)
+print("Underlying values: %s"%list(labels))
