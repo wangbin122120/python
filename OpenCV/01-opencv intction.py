1e54967d50b0da42bc2f5306326ea8833dc19092
wangbin122120 <wangbin122120@163.com>
2017-09-05 16:43:07 +0800

    99

--- OpenCV/01-opencv intction.py
+++ OpenCV/01-opencv intction.py
@@ -6,3 +6,12 @@ img = cv2.imread('image/watch.jpg',cv2.IMREAD_GRAYSCALE)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+img = cv2.imread('image/watch.jpg',cv2.IMREAD_GRAYSCALE)
+
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.plot([200,300,400],[100,200,300],'c', linewidth=5)
+plt.show()
