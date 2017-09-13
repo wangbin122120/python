51ee39388720ef98f09cf174edfa5898e457a684
wangbin122120 <wangbin122120@163.com>
2017-09-14 00:37:31 +0800

    move

--- 十三种TensorFlow实现案例/m.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-with tf.Session() as sess:
-    print(sess.run(tf.reduce_sum(tf.add([[1], [2], [4]], [[4], [1], [3]]), axis=-1)))
-    print(sess.run(tf.reduce_sum(tf.add([[1], [2], [4]], [[4], [1], [3]]), axis=0)))
-
-
