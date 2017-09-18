2fbe18bd1e3556f5851391df03e61d938cf97f05
wangbin122120 <wangbin122120@163.com>
2017-09-18 23:23:48 +0800

    notebook

---
+++ Machine Learning/sentdex/01-金融数据训练和预测.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import quandl  # 用于下载金融数据
+import numpy as np  # 用于将 get到的数据 序列化成 numpy.
+from sklearn import preprocessing  # 数据标准化，归一化 ，参考：http://blog.csdn.net/dream_angel_z/article/details/49406573
+from sklearn import svm  # 支持向量机，svm训练和预测模型 http://blog.csdn.net/v_july_v/article/details/7624837
+from sklearn.linear_model import LinearRegression  # 线性回归，在本次训练中效果较好。
+from sklearn.model_selection import train_test_split # 训练集和测试集的划分。
+import datetime  # 用于绘制plot()图的横坐标
+import matplotlib.pyplot as plt # 绘图
+from matplotlib import style # 绘图
+import pickle  # 绘图
+
+'''
+本程序仅仅是提供了sklearn 如何训练和预测数据，并且对结果进行展示，这个例子并不能实际用于生产。
+因为金融股票数据的波动，是受到各种因素的影响，并且每类每个公司的影响因素和受影响程度是非常不同。
+
+教程文字版：https://pythonprogramming.net/regression-introduction-machine-learning-tutorial/
+
+'''
+
+
+# 1. 下载数据，并且定义了两个辅助列，定义用于数据集合。
+df = quandl.get(dataset='WIKI/GOOGL', api_key='6yWSw4q4iEWbsU-SspQ5')
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+# 2. 制造预测结果。
+forecast_col = 'Adj. Close'
+df.fillna(-9999, inplace=True)  # 如果存在空值，则用-9999填充。数据清洗。
+forecast_out = int(0.01 * len(df))
+print('一共%d个交易日，预测%d个' % (len(df), forecast_out))
+last_ = df[forecast_col][-forecast_out:]
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)  # 我们将空值去除（也就是最后的空值啦，因为前面已经对空值做了-9999的操作）
+
+# 3. 数据标准化，划分训练和测试数据。
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)   # 标准化（Standardization）， (x-均值)/方差
+y = np.array(df['label'])
+X_lately = X[-forecast_out:]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# 4. training and evaluation
+# clf = svm.SVR()           # 用svm模型 confidence=0.84,实际画图差异挺大。
+clf = svm.SVR(kernel='linear') # svm 中的线性模型结果也很不错，confidence : 0.976
+# clf = LinearRegression()    # 用线性模型训练 confidence=0.97
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test) #  1 -  ((y_true - y_pred) ** 2).sum() /  ((y_true - y_true.mean()) ** 2).sum()
+# 或者从保存的训练模型直接恢复，那么不需要定义、训练，直接就可以用于预测：
+# clf = pickle.load(open('LinearRegression.pickle', 'rb'))
+
+forecast_set = clf.predict(X_lately)
+print('confidence :',confidence) # 这里如果叫准确率有点不妥，毕竟不是分类模型。
+# for i in range(len(forecast_set)):
+#     print(X_lately[i], forecast_set[i], last_[i])
+
+# 5. 绘图
+style.use('ggplot')  # 美化输出结果，而且还很方便的使用放大工具
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()  # 这段是之前的走势。
+df['Forecast'].plot()  # 这段是预测走势
+last_.plot()  # 这段是真实的走势
+
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
+# 6. 保存训练结果，下次直接调用就可以做预测，一般在预测结束之后，预测开始之前进行保存。
+# 保存
+with open('LinearRegression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+# # 提取
+# pickle_in = open('LinearRegression.pickle', 'rb')
+# clf_2 = pickle.load(pickle_in)
+# # 预测，并且结果和之前的一至
+# forecast_set2 = clf_2.predict(X_lately)
+# print(forecast_set == forecast_set2)
+
+
+
--- Machine Learning/sentdex/m.py
+++ Machine Learning/sentdex/m.py
@@ -1,58 +1,196 @@
-import pandas as pd
-import quandl
+# 1. 主要是说 预测模型要符合数据分布，预测效果才会准确，并且介绍了，线性回归模型的参数计算公式。
+#  Regression - Theory and how it works
+# https://pythonprogramming.net/simple-linear-regression-machine-learning-tutorial/?completed=/pickling-scaling-machine-learning-tutorial/
+# https://www.youtube.com/watch?v=SvmueyhSkgQ&index=8&list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v
 
 
-df = quandl.get('WIKI/GOOGL')
+# 2. 用python 计算线性回归模型的斜率 slope
+# https://pythonprogramming.net/how-to-program-best-fit-line-slope-machine-learning-tutorial/?completed=/simple-linear-regression-machine-learning-tutorial/
+from statistics import mean
+import numpy as np
 
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-
-print('1.加载数据')
-print(df.head())
-print(df.tail())
-print(df.shape)
-
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
-df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
-
-df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-
-print('2.添加 pct 和 pct_change')
-print(df.head())
-print(df.tail())
-print(df.shape)
+xs = np.array([1, 2, 3, 4, 5], dtype=np.float64)
+ys = np.array([5, 4, 6, 5, 6], dtype=np.float64)
 
 
-
-forecast_col = 'Adj. Close'
-df.fillna(-9999,inplace=True)   #如果存在空值，则用-9999填充。数据清洗。
-
-# 将价格移动一下，用shift(-N) 将未来N个交易日的值拉回当前。 这里N=df/100=38，所以在最后的38个数字是空的。而前面不是空值，所以用这些作为预测值，以供学习。
-import math
-forecast_out=int(math.ceil(0.01*len(df)))
-df['label']=df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)   # 我们将空值去除（也就是最后的空值啦，因为前面已经对空值做了-9999的操作）
-
-print('3.新增一个预测价格的列label')
-print(df.tail())
-print(df.shape)
-''' 所以这里展示的label是未来33个交易日的价格，
-            Adj. Close    HL_PCT  PCT_change  Adj. Volume   label （未来33个交易日的价格）
-Date                                                             
-2017-07-25      969.03  0.794609   -0.172041    5793414.0  943.29  
-2017-07-26      965.31  0.895049   -0.767902    2166225.0  946.65  
-2017-07-27      952.51  1.785808   -1.720011    3685905.0  950.44   
-2017-07-28      958.33  0.361045    1.090729    1795477.0  940.13 （2017-09-14）
-2017-07-31      945.50  1.659704   -1.510417    2268160.0  935.29 （2017-09-15）
-'''
+def best_fit_slope(xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
+         ((mean(xs) ** 2) - mean(xs ** 2)))
+    return m
 
 
+m = best_fit_slope(xs, ys)
+print(m)
+
+# 3. 通过上面的介绍，继续计算 线性回归模型的 参数b,然后完成预测，并将训练数据和回归直线、预测结果都图像化。 best_fit_slope_and_intercept()
+# How to program the Best Fit Line - Practical Machine Learning Tutorial with Python p.9
+#
+print('# 3. How to program the Best Fit Line')
+from statistics import mean
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import style
 
 
+def best_fit_slope_and_intercept(xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
+         ((mean(xs) * mean(xs)) - mean(xs * xs)))
+
+    b = mean(ys) - m * mean(xs)
+
+    return m, b
 
 
+xs = np.array([1, 2, 3, 4, 5], dtype=np.float64)
+ys = np.array([5, 4, 6, 5, 6], dtype=np.float64)
+
+m, b = best_fit_slope_and_intercept(xs, ys)
+
+regression_line = [(m * x) + b for x in xs]
+
+predict_x = 7
+predict_y = (m * predict_x) + b
+print(m, b)
+print(predict_x, predict_y)
+
+style.use('ggplot')
+# style.use('fivethirtyeight')  # 分别感受一下不同的图形界面。
+plt.scatter(xs, ys, color='#003F72')
+plt.scatter(predict_x, predict_y, color='r')
+plt.plot(xs, regression_line)
+plt.show()
+
+# 4. 介绍R方 理论，用于评估回归模型的拟合程度。R Squared Theory - Practical Machine Learning Tutorial with Python p.10
+# https://pythonprogramming.net/r-squared-coefficient-of-determination-machine-learning-tutorial/?completed=/how-to-program-best-fit-line-machine-learning-tutorial/
+# https://youtu.be/-fgYp74SNtk?list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v
+print('# 4. R Squared Theory - Practical Machine Learning Tutorial with Python p.10')
+
+# 5. R方的python 计算实现： coefficient_of_determination()
+# https://pythonprogramming.net/how-to-program-r-squared-machine-learning-tutorial/?completed=/r-squared-coefficient-of-determination-machine-learning-tutorial/
+# https://youtu.be/QUyAFokOmow?list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v
+
+from statistics import mean
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import style
 
 
+def best_fit_slope_and_intercept(xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
+         ((mean(xs) * mean(xs)) - mean(xs * xs)))
+
+    b = mean(ys) - m * mean(xs)
+
+    return m, b
 
 
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig) * (ys_line - ys_orig))
 
 
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+
+xs = np.array([1, 2, 3, 4, 5], dtype=np.float64)
+ys = np.array([5, 4, 6, 5, 6], dtype=np.float64)
+
+m, b = best_fit_slope_and_intercept(xs, ys)
+regression_line = [(m * x) + b for x in xs]
+
+r_squared = coefficient_of_determination(ys, regression_line)
+print('线性回归模型：\' y = %.4f * x + %.4f \' , R^2=%4f' % (m, b, r_squared))
+
+predict_x = 7
+predict_y = (m * predict_x) + b
+print('预测 x = %f  -> y = %f ' % (predict_x, predict_y))
+
+style.use('ggplot')
+# style.use('fivethirtyeight')  # 分别感受一下不同的图形界面。
+plt.scatter(xs, ys, color='#003F72')
+plt.scatter(predict_x, predict_y, color='r')
+plt.plot(xs, regression_line)
+plt.show()
+
+# 6. 随机生成数据集，进行测试。
+# Testing Assumptions - Practical Machine Learning Tutorial with Python p.12
+# https://youtu.be/Kpxwl2u-Wgk?list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v
+# https://pythonprogramming.net/sample-data-testing-machine-learning-tutorial/?completed=/how-to-program-r-squared-machine-learning-tutorial/
+
+print('# 6. Testing Assumptions - Practical Machine Learning Tutorial with Python p.12')
+from statistics import mean
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+import random
+
+def best_fit_slope_and_intercept(xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
+         ((mean(xs) * mean(xs)) - mean(xs * xs)))
+
+    b = mean(ys) - m * mean(xs)
+
+    return m, b
+
+
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig) * (ys_line - ys_orig))
+
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+
+# 6.
+# hm 测试点数量，variance 随机波动范围，偏离直线最大距离，step 是相当于斜率，correlation 相关性，pos 斜率为正，neg向下，否则不相干，那么数据就完全是一个区域内随机生成。
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
+
+
+# 某种程度上说，
+# xs, ys = create_dataset(40, 40, 2, correlation='pos')
+xs, ys = create_dataset(40,10,2,correlation='pos')
+# xs, ys = create_dataset(40,10,2,correlation='neg')
+# xs, ys = create_dataset(40,10,2,correlation=False)
+# 用自动生成是数据替代。
+# xs = np.array([1, 2, 3, 4, 5], dtype=np.float64)
+# ys = np.array([5, 4, 6, 5, 6], dtype=np.float64)
+
+m, b = best_fit_slope_and_intercept(xs, ys)
+regression_line = [(m * x) + b for x in xs]
+
+r_squared = coefficient_of_determination(ys, regression_line)
+print('线性回归模型：\' y = %.4f * x + %.4f \' , R^2=%4f' % (m, b, r_squared))
+
+predict_x = 7
+predict_y = (m * predict_x) + b
+print('预测 x = %f  -> y = %f ' % (predict_x, predict_y))
+
+style.use('ggplot')
+# style.use('fivethirtyeight')  # 分别感受一下不同的图形界面。
+plt.scatter(xs, ys, color='#003F72')
+plt.scatter(predict_x, predict_y, color='r')
+plt.plot(xs, regression_line)
+plt.show()
